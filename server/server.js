2fdc37b0ecc0c3229b9cba2117d54efa82845013
Ashish Jain <Ashish.jain8847@gmail.com>
2018-05-28 11:28:20 +0530

    My first commit for Exercise1 app

---
+++ server/server.js
@@ -0,0 +1,59 @@
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['account', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+  //   return user.generateAuthToken();
+  // }).then((token) => {
+  //   res.header('x-auth', token).send(user);
+  res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['account', 'password']);
+
+  User.findByCredentials(body.account, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,8 @@
+const expect = require('expect');
+const request = require('supertest');
+const {ObjectID} = require('mongodb');
+
+const {app} = require('./../server');
+const {User} = require('./../models/user');
+
+beforeEach(populateUsers);
